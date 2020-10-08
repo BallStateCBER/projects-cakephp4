9aa362692d4497329553f1f29cc99e6e4658948d
Graham Watson <graham@phantomwatson.com>
2020-10-08 11:40:42 -0400

    Add subsite title

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -6,7 +6,12 @@ $this->extend('DataCenter.default');
 $this->assign('sidebar', $this->element('sidebar'));
 ?>
 
-<?php $this->append('scriptBottom'); ?>
+<?php $this->append('subsite_title'); ?>
+    <h1 id="subsite_title" class="max_width">
+        <a href="/">
+            <img src="/img/banner.jpg" alt="Projects and Publications" />
+        </a>
+    </h1>
 <?php $this->end(); ?>
 
 <div id="content">
