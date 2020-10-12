d3306b49983bd2e3312683e22630328c938b6a77
Graham Watson <graham@phantomwatson.com>
2020-10-12 12:02:21 -0400

    Extend DataCenterView
    This delegates form template setting to the plugin

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  */
 namespace App\View;
 
-use Cake\View\View;
+use DataCenter\View\DataCenterView;
 
 /**
  * Application View
@@ -24,7 +24,7 @@ use Cake\View\View;
  *
  * @link https://book.cakephp.org/4/en/views.html#the-app-view
  */
-class AppView extends View
+class AppView extends DataCenterView
 {
     /**
      * Initialization hook method.
@@ -37,5 +37,6 @@ class AppView extends View
      */
     public function initialize(): void
     {
+        parent::initialize();
     }
 }
