9c92f0911ac908e637de7b9d370990cfdac640ca
Graham Watson <graham@phantomwatson.com>
2021-01-15 17:35:54 -0500

    Add siteTitle and siteUrl config values

--- config/datacenter.php
+++ config/datacenter.php
@@ -4,5 +4,7 @@ return [
         'auth' => [
             'enabled' => true,
         ],
+        'siteTitle' => 'CBER Projects and Publications',
+        'siteUrl' => 'https://projects.cberdata.org',
     ],
 ];
