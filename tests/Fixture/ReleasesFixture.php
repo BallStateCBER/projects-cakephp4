109df50b26eca5beffe7a6d35ff98b887b1ed50d
Graham Watson <graham@phantomwatson.com>
2021-01-19 18:21:03 -0500

    Fix slug not matching pattern defined in routes.php

--- tests/Fixture/ReleasesFixture.php
+++ tests/Fixture/ReleasesFixture.php
@@ -45,7 +45,7 @@ class ReleasesFixture extends TestFixture
             [
                 'id' => 1,
                 'title' => 'Lorem ipsum dolor sit amet',
-                'slug' => 'Lorem ipsum dolor sit amet',
+                'slug' => 'lorem-ipsum-dolor-sit-amet',
                 'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
                 'released' => '2020-09-23',
                 'partner_id' => 1,
