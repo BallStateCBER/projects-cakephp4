16359856795a1ab4232a972d1589892497ab5f45
Graham Watson <graham@phantomwatson.com>
2021-01-19 17:02:13 -0500

    Add DataCenter plugin config values

--- config/datacenter.php
+++ config/datacenter.php
@@ -3,8 +3,28 @@ return [
     'DataCenter' => [
         'auth' => [
             'enabled' => true,
+            'loginUrl' => [
+                'prefix' => false,
+                'plugin' => null,
+                'controller' => 'Users',
+                'action' => 'login',
+            ],
+            'logoutRedirect' => [
+                'prefix' => false,
+                'plugin' => null,
+                'controller' => 'Users',
+                'action' => 'login',
+            ],
         ],
         'siteTitle' => 'CBER Projects and Publications',
         'siteUrl' => 'https://projects.cberdata.org',
+
+        // Use full URL or leading slash, e.g. '/img/logo/og_logo.png'
+        'defaultOpenGraphLogoPath' => '',
+        'defaultTwitterLogoPath' => '',
+
+        'openGraphDescription' => 'Ball State University\'s Center for Business and Economic Research is a federally designated state data center, providing public access to official government data and other trusted primary sources. We also provide insight and context for what these numbers mean and publish a wide variety of unbiased, data-driven projects and publications that are useful to policy makers, industry experts, community leadership, consultants, and informed citizens.',
+        'facebookAppId' => '',
+        'twitterUsername' => '@BallStateCBER',
     ],
 ];
