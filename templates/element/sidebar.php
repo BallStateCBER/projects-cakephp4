7903a4438fd63458af8b9266d0ae5bf3cf0bcdb8
Graham Watson <graham@phantomwatson.com>
2021-02-05 18:35:17 -0500

    Responsively collapse sidebar sections

--- templates/element/sidebar.php
+++ templates/element/sidebar.php
@@ -5,119 +5,156 @@
  */
 ?>
 <?php if ($sidebarVars['user']): ?>
-    <h2>
-        Administration
-    </h2>
-    <ul class="unstyled">
-        <li>
-            <?= $this->Html->link(
-                'New Release',
-                [
-                    'plugin' => false,
-                    'controller' => 'Releases',
-                    'action' => 'add',
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Releases',
-                [
-                    'plugin' => false,
-                    'controller' => 'Releases',
-                    'action' => 'admin',
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Users',
-                [
-                    'plugin' => false,
-                    'controller' => 'Users',
-                    'action' => 'index',
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Partners',
-                [
-                    'plugin' => false,
-                    'controller' => 'Partners',
-                    'action' => 'index',
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Tags',
-                [
-                    'plugin' => false,
-                    'controller' => 'Tags',
-                    'action' => 'index',
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Authors',
-                [
-                    'plugin' => false,
-                    'controller' => 'Authors',
-                    'action' => 'index',
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Update login info',
-                [
-                    'plugin' => false,
-                    'controller' => 'Users',
-                    'action' => 'edit',
-                    $sidebarVars['user']['id'],
-                ]
-            ) ?>
-        </li>
-        <li>
-            <?= $this->Html->link(
-                'Logout',
-                [
-                    'plugin' => false,
-                    'controller' => 'Users',
-                    'action' => 'logout',
-                ]
-            ) ?>
-        </li>
-    </ul>
+    <section class="navbar-light navbar-expand-md sidebar-collapse">
+        <div class="row">
+            <div class="col">
+                <h2 data-toggle="collapse" data-target="#sidebar-admin">
+                    Administration
+                </h2>
+            </div>
+            <div class="col-2">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebar-admin"
+                        aria-controls="sidebar-admin" aria-expanded="false" aria-label="Toggle administration links">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
+        <ul class="unstyled collapse navbar-collapse" id="sidebar-admin">
+            <li>
+                <?= $this->Html->link(
+                    'New Release',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Releases',
+                        'action' => 'add',
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Releases',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Releases',
+                        'action' => 'admin',
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Users',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Users',
+                        'action' => 'index',
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Partners',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Partners',
+                        'action' => 'index',
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Tags',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Tags',
+                        'action' => 'index',
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Authors',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Authors',
+                        'action' => 'index',
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Update login info',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Users',
+                        'action' => 'edit',
+                        $sidebarVars['user']['id'],
+                    ]
+                ) ?>
+            </li>
+            <li>
+                <?= $this->Html->link(
+                    'Logout',
+                    [
+                        'plugin' => false,
+                        'controller' => 'Users',
+                        'action' => 'logout',
+                    ]
+                ) ?>
+            </li>
+        </ul>
+    </section>
 <?php endif; ?>
 
-<h2>
-    Search
-</h2>
-<?= $this->Form->create(
-    null,
-    [
-        'method' => 'get',
-        'url' => [
-            'plugin' => false,
-            'controller' => 'Releases',
-            'action' => 'search',
-        ],
-    ]
-) ?>
-<?= $this->Form->control('term', ['label' => false]) ?>
-<?= $this->Form->submit('Search') ?>
-<?= $this->Form->end() ?>
+<section class="navbar-light navbar-expand-md sidebar-collapse">
+    <div class="row">
+        <div class="col">
+            <h2 data-toggle="collapse" data-target="#sidebar-search">
+                Search
+            </h2>
+        </div>
+        <div class="col-2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebar-search"
+                    aria-controls="sidebar-search" aria-expanded="false" aria-label="Toggle search form">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse" id="sidebar-search">
+        <?= $this->Form->create(
+            null,
+            [
+                'method' => 'get',
+                'url' => [
+                    'plugin' => false,
+                    'controller' => 'Releases',
+                    'action' => 'search',
+                ],
+            ]
+        ) ?>
+        <?= $this->Form->control('term', ['label' => false]) ?>
+        <?= $this->Form->submit('Search', ['class' => 'btn btn-secondary']) ?>
+        <?= $this->Form->end() ?>
+    </div>
+</section>
 
-<h2>
-    Topics
-</h2>
-<ul class="tags unstyled" id="tags-list">
-    <?php foreach ($sidebarVars['tags'] as $tag): ?>
-        <li>
-            <?php
+<section class="navbar-light navbar-expand-md sidebar-collapse">
+    <div class="row">
+        <div class="col">
+            <h2 data-toggle="collapse" data-target="#tags-list">
+                Topics
+            </h2>
+        </div>
+        <div class="col-2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#tags-list"
+                    aria-controls="tags-list" aria-expanded="false" aria-label="Toggle topic links">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <ul class="tags unstyled collapse navbar-collapse" id="tags-list">
+        <?php foreach ($sidebarVars['tags'] as $tag): ?>
+            <li>
+                <?php
                 echo $this->Html->link($tag->uc_name, [
                     'plugin' => false,
                     'controller' => 'Tags',
@@ -125,41 +162,67 @@
                     'id' => $tag->id,
                     'slug' => $tag->slug,
                 ]);
-            ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+                ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
 
-<h2>
-    Clients, Partners, and Sponsors
-</h2>
-<ul class="partners unstyled" id="partners-list">
-    <?php foreach ($sidebarVars['partners'] as $partner): ?>
-        <li>
-            <?= $this->Html->link(
-                $partner->short_name,
-                $partner->url,
-                ['title' => $partner->name]
-            ) ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<section class="navbar-light navbar-expand-md sidebar-collapse">
+    <div class="row">
+        <div class="col">
+            <h2 data-toggle="collapse" data-target="#partners-list">
+                Clients, Partners, and Sponsors
+            </h2>
+        </div>
+        <div class="col-2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#partners-list"
+                    aria-controls="partners-list" aria-expanded="false"
+                    aria-label="Toggle clients, partners, and sponsors links">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <ul class="partners unstyled collapse navbar-collapse" id="partners-list">
+        <?php foreach ($sidebarVars['partners'] as $partner): ?>
+            <li>
+                <?= $this->Html->link(
+                    $partner->short_name,
+                    $partner->url,
+                    ['title' => $partner->name]
+                ) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
 
-<h2>
-    Publishing Date
-</h2>
-<ul class="unstyled" id="years-list">
-    <?php foreach ($sidebarVars['years'] as $year): ?>
-        <li>
-            <?= $this->Html->link($year, [
-                'plugin' => false,
-                'controller' => 'Releases',
-                'action' => 'year',
-                'year' => $year,
-            ]) ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<section class="navbar-light navbar-expand-md sidebar-collapse">
+    <div class="row">
+        <div class="col">
+            <h2 data-toggle="collapse" data-target="#years-list">
+                Publishing Date
+            </h2>
+        </div>
+        <div class="col-2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#years-list"
+                    aria-controls="years-list" aria-expanded="false" aria-label="Toggle publishing year links">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <ul class="unstyled collapse navbar-collapse" id="years-list">
+        <?php foreach ($sidebarVars['years'] as $year): ?>
+            <li>
+                <?= $this->Html->link($year, [
+                    'plugin' => false,
+                    'controller' => 'Releases',
+                    'action' => 'year',
+                    'year' => $year,
+                ]) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
 
 <?php if (!$sidebarVars['user']): ?>
     <?= $this->Html->link(
