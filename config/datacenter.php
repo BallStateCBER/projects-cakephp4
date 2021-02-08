2d4dde24ec6707c39336670ec4a42fccad21d083
Graham Watson <graham@phantomwatson.com>
2021-02-08 17:51:27 -0500

    Add Google Analytics config data

--- config/datacenter.php
+++ config/datacenter.php
@@ -26,5 +26,6 @@ return [
         'openGraphDescription' => 'Ball State University\'s Center for Business and Economic Research is a federally designated state data center, providing public access to official government data and other trusted primary sources. We also provide insight and context for what these numbers mean and publish a wide variety of unbiased, data-driven projects and publications that are useful to policy makers, industry experts, community leadership, consultants, and informed citizens.',
         'facebookAppId' => '',
         'twitterUsername' => '@BallStateCBER',
+        'googleAnalyticsId' => 'G-ZXVVDVTS89',
     ],
 ];
