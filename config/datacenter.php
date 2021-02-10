1265320a3379d3cd788edf74a5832e5c58a29d0e
Graham Watson <graham@phantomwatson.com>
2021-02-10 13:28:57 -0500

    Comment out config values that should fall back to defaults

--- config/datacenter.php
+++ config/datacenter.php
@@ -20,11 +20,11 @@ return [
         'siteUrl' => 'https://projects.cberdata.org',
 
         // Use full URL or leading slash, e.g. '/img/logo/og_logo.png'
-        'defaultOpenGraphLogoPath' => '',
-        'defaultTwitterLogoPath' => '',
+        //'defaultOpenGraphLogoPath' => '',
+        //'defaultTwitterLogoPath' => '',
 
         'openGraphDescription' => 'Ball State University\'s Center for Business and Economic Research is a federally designated state data center, providing public access to official government data and other trusted primary sources. We also provide insight and context for what these numbers mean and publish a wide variety of unbiased, data-driven projects and publications that are useful to policy makers, industry experts, community leadership, consultants, and informed citizens.',
-        'facebookAppId' => '',
+        //'facebookAppId' => '',
         'twitterUsername' => '@BallStateCBER',
         'googleAnalyticsId' => 'G-ZXVVDVTS89',
     ],
