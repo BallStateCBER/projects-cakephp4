acd8ba031be25c9c49e5996878743edfb8475795
Graham Watson <graham@phantomwatson.com>
2021-03-03 15:41:12 -0500

    Add testEditFormContents()

--- tests/TestCase/Controller/ReleasesControllerTest.php
+++ tests/TestCase/Controller/ReleasesControllerTest.php
@@ -455,6 +455,50 @@ class ReleasesControllerTest extends TestCase
     }
 
     /**
+     * Test that the edit form is pre-populated with all expected contents
+     *
+     * @return void
+     */
+    public function testEditFormContents(): void
+    {
+        $this->setUserSession();
+        $releaseId = ReleasesFixture::RELEASE_WITH_GRAPHICS;
+
+        $release = $this->Releases->get(
+            $releaseId,
+            ['contain' => ['Authors', 'Graphics', 'Partners', 'Tags']]
+        );
+
+        $this->get($this->editUrl . $releaseId);
+        $this->assertResponseOk();
+
+        $selectedTags = [];
+        foreach ($release->tags as $tag) {
+            $selectedTags[] = [
+                'id' => $tag->id,
+                'name' => $tag->name,
+            ];
+            $this->assertResponseContains('"selectedTags":' . json_encode($selectedTags));
+        }
+        foreach ($release->authors as $author) {
+            $selectedAuthorInput = sprintf(
+                '<input type="hidden" name="authors[_ids][]" value="%s"',
+                $author->id,
+            );
+            $this->assertResponseContains($selectedAuthorInput);
+        }
+        foreach ($release->graphics as $graphic) {
+            $this->assertResponseContains($graphic->title);
+        }
+        $selectedPartnerOption = sprintf(
+            '<option value="%s" selected="selected">%s</option>',
+            $release->partner->id,
+            $release->partner->name,
+        );
+        $this->assertResponseContains($selectedPartnerOption);
+    }
+
+    /**
      * Test delete method
      *
      * @return void
